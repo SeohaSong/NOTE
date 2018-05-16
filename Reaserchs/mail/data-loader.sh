4845f49bc07651c0595fdf91871d75a683d950de
seohasong <tisutoo@gmail.com>
2018-05-16 21:01:11 +0900

    update origin master

--- Reaserchs/mail/data-loader.sh
+++
@@ -1,5 +0,0 @@
-curl -o cifar-10-python.tar.gz \
-https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xvf cifar-10-python.tar.gz
-mv cifar-10-batches-py data
-rm -rf cifar-10-python.tar.gz
